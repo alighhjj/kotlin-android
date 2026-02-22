12e49e5ce4974f52e4135819b13615c64b55eb45
alighhjj <zhaoyjgzs@sina.com>
2026-02-22 14:05:00 +0800

    修复工作流错误：移除过时的kotlinOptions块

--- library-imagemagick/build.gradle.kts
+++ library-imagemagick/build.gradle.kts
@@ -29,10 +29,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     sourceSets {
         getByName("main") {
             jniLibs.srcDirs(listOf("src/main/jniLibs"))
